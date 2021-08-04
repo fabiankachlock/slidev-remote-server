892e709e14a672921cfba4f34f165faf278109a0
fabiankachlock <fabian.kachlock@icloud.com>
2021-08-04 13:51:05 +0200

    Added Vue

---
+++ src/frontend/components/Test.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>Hello World</h1>
+  <div>
+    <button @click="counter++">++</button>
+    <span>
+      {{ counter }}
+    </span>
+    <button @click="counter--">--</button>
+  </div>
+  <p>Test + {{ counter }} {{ abc() }}</p>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+function abc() {
+  return 'test';
+}
+
+export default defineComponent({
+  name: 'Test',
+  methods: {
+    abc
+  },
+  setup() {
+    const counter = ref(0);
+    return { counter };
+  }
+});
+</script>
